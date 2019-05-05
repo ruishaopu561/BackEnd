be5dfa41c119d817df93b0120ce479fe54903609
rsp <1747836307@qq.com>
2019-05-05 18:48:49 +0800

    update and finished

--- homework3/homework3/src/main/java/rsp/homework3/WordladderController.java
+++ homework3/homework3/src/main/java/rsp/homework3/WordladderController.java
@@ -1,46 +1,34 @@
 package rsp.homework3;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.http.*;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
+// import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.*;
-
-@Controller
-@EnableAutoConfiguration
+@RestController
 public class WordladderController {
 
     @RequestMapping("/ladder")
-    public String ladder(@RequestParam(value = "u") String username,
-                            @RequestParam(value = "p") String password,
-                            @RequestParam(value = "b") String begin,
-                            @RequestParam(value = "e") String end) {
-
+    public String ladder(@RequestParam(value = "u") String username, @RequestParam(value = "p") String password,
+            @RequestParam(value = "b") String begin, @RequestParam(value = "e") String end) {
 
         System.out.println(username);
         System.out.println(password);
-        //验证用户权限过程
-//        String userName = "admin";
-//        String passWord = "123";
+        // 验证用户权限过程
         RestTemplate rt = new RestTemplate();
-        String url = "http://localhost/8600/login?u="+username+"&p="+password;
+        String url = "http://localhost:8900/login?u={1}&p={2}";
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-//        HttpEntity<String> entity = new HttpEntity<String>(body, headers);
+        String response = rt.getForObject(url, String.class, username, password);
 
-        ResponseEntity<String> response = rt.getForEntity(url, String.class);
+        System.out.println(response);
+        // 验证通过，给出结果
 
-        String responsedata = response.getBody();
-
-        System.out.println(responsedata);
-        //验证通过，给出结果
-
-        Wordladder wl = new Wordladder();
-        return wl.ss(begin,end);
+        if (response.equals("true")) {
+            Wordladder wl = new Wordladder();
+            return wl.ss(begin, end);
+        } else {
+            return "User not exist.";
+        }
     }
 }
--- homework3/login/src/main/java/rsp/login/LoginController.java
+++ homework3/login/src/main/java/rsp/login/LoginController.java
@@ -1,19 +1,17 @@
 package rsp.login;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@Controller
-@EnableAutoConfiguration
+@RestController
+@CrossOrigin
 public class LoginController {
 
     @RequestMapping("/login")
-    public String getLogin(@RequestParam(value = "u") String username, @RequestParam(value = "p") String password)
-    {
-        if (username.equals("admin") && password.equals("123"))
-        {
+    public String getLogin(@RequestParam(value = "u") String username, @RequestParam(value = "p") String password) {
+        if (username.equals("admin") && password.equals("123")) {
             return "true";
         } else {
             return "false";
